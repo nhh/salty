3cf76ee91407d2054702d4687b21a3d4f80e7dbb
nhh <Niklas.Hanft@outlook.com>
2018-11-11 18:30:54 +0100

    Initial commit

---
+++ spec/walter_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Walter do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/walter.cr
@@ -0,0 +1,10 @@
+require "walter/*"
+
+module Walter
+  VERSION = "0.1.0"
+  HANDLERS = [] of Walter::Server
+
+  configs = Walter::Configuration
+
+
+end
