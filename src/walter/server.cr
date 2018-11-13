4d606da7e7678bc57d42a982253bc3290ef62920
nhh <Niklas.Hanft@outlook.com>
2018-11-13 08:14:05 +0100

    Refactor to io streaming
    We are not there yet, but all io might be available now for response streaming

--- src/walter/server.cr
+++ src/walter/server.cr
@@ -19,23 +19,17 @@ module Walter
 
       spawn do
         server = HTTP::Server.new do |context|
-          request = HTTP::Request.new(
-            context.request.method, 
-            context.request.resource, 
-            context.request.headers, 
-            context.request.body, 
-            version = "HTTP/1.1"
-          )
-          puts "[INFO] Working on #{request} with #{context}"
-          client = HTTP::Client.new @configuration.vhosts[0].target, @configuration.vhosts[0].port
-          response = client.exec(request)
-          client.close
-          client = nil
 
-          context.response.content_type = "text/html"
-          context.response.print response.body
-          context.response.close
-          context.request
+          client = HTTP::Client.new @configuration.vhosts[0].target, @configuration.vhosts[0].port
+
+          client.exec(context.request.method, context.request.resource, context.request.headers, context.request.body) do |response|
+            context.response.content_type = "text/html"
+            context.response.print(response.body_io.gets)
+            context.response.close
+          end
+
+          client.close
+
         end
         puts "Listening on http://0.0.0.0:#{@configuration.port.to_s}"
         server.listen(@configuration.port.to_i, true)
